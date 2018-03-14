e55421b0ab7cb0cf0890994d2b9d7c6c62bdd545
cez.bloch@gmail.com <cezary.bloch@displaylink.com>
2018-03-14 16:28:42 +0100

    Adding size function to Texture2D so that clients can ask about the size of the frame during setup. Instatiating Textre2D template for float4, int4 and uint 4.

--- Shaderator/Texture2D.h
+++ Shaderator/Texture2D.h
@@ -9,12 +9,13 @@ class Texture2D
 public:
   Texture2D() = default;
   Texture2D(unsigned int _width, unsigned int _height);
+  Texture2D(uint2 size);
   T& operator [](uint2 coordinates);
   T& Load(uint3 coordinates);
   const void* data();
+  const uint2 size();
 
 private:
-  unsigned int m_width;
-  unsigned int m_height;
+  uint2 m_size;
   std::vector<T> m_data;
 };
